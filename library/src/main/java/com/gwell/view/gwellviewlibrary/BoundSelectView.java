71bb3ec764c8585783f9f7778aa1c9d286c93343
lianshangyangguang <1141179268@qq.com>
2017-05-04 21:33:18 +0800

    修改subCount的数据获取

--- library/src/main/java/com/gwell/view/gwellviewlibrary/BoundSelectView.java
+++ library/src/main/java/com/gwell/view/gwellviewlibrary/BoundSelectView.java
@@ -269,7 +269,7 @@ public class BoundSelectView extends ViewGroup {
         this.itemOnClickListener = itemOnClickListener;
         if (names != null && names.size() != 0) {
 
-            subCount =  names.size();
+            subCount = names.size();
             //由数据决定显示的条目数
             initData();
             for (int i = 0; i < subCount; i++) {
